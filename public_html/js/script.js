a985e07fe196a9dec26217bf104e9aa9638a7478
ashik <ashik@studevs>
2017-08-06 15:24:08 +0600

    Update : Imported basic controller and filters.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,7 +1,16 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+var app = angular.module('app', []);
 
+app.controller('controllerTest', function controllerTest($scope) {
+
+    $scope.students = [
+        {'name': 'Mohammad Ashikuzzaman', 'tag': 'Developer'},
+        {'name': 'Ariful Islam Badal', 'tag': 'Navy Officer'},
+        {'name': 'Srijan Mazumder Michael', 'tag': 'Planner'}
+    ];
+});
